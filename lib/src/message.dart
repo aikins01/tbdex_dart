07f5a23ec032e3be3519ecd48f6c99d4395d4ed6
Aikins Laryea <aikinslaryea@gmail.com>
2024-09-04 11:52:55 +0000

    chore: added message classes

---
+++ lib/src/message.dart
@@ -0,0 +1,59 @@
+import 'package:typeid/typeid.dart';
+
+enum MessageKind {
+  rfq,
+  cancel,
+  close,
+  quote,
+  order,
+  orderInstructions,
+  orderStatus,
+}
+
+abstract class MessageData {
+  MessageKind kind();
+}
+
+class MessageMetadata {
+  final TypeId id;
+  final MessageKind kind;
+  final String from;
+  final String to;
+  final String exchangeId;
+  final DateTime createdAt;
+  final String? externalId;
+  final String protocol;
+
+  MessageMetadata({
+    required this.id,
+    required this.kind,
+    required this.from,
+    required this.to,
+    required this.exchangeId,
+    required this.createdAt,
+    this.externalId,
+    required this.protocol,
+  });
+}
+
+class Message<D extends MessageData> {
+  final MessageMetadata metadata;
+  final D data;
+  String? signature;
+
+  Message({
+    required this.metadata,
+    required this.data,
+    this.signature,
+  });
+
+  Future<bool> verify() async {
+    // TODO: Implement verification logic
+    throw UnimplementedError();
+  }
+
+  Future<void> sign() async {
+    // TODO: Implement signing logic
+    throw UnimplementedError();
+  }
+}
